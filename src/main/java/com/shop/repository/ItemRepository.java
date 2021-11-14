865f65e1635978c96b400e3305d8927745fbeba1
youns <minys94@daum.net>
2021-11-14 21:41:47 +0900

    상품 조건 조회 쿼리 메서드 추가

--- src/main/java/com/shop/repository/ItemRepository.java
+++ src/main/java/com/shop/repository/ItemRepository.java
@@ -7,7 +7,14 @@ import java.util.List;
 
 public interface ItemRepository extends JpaRepository<Item, Long>{
 
-    List<Item> findByItemNm(String itemNm);
+    List<Item> findByItemNm(String itemNm); // 아이템 이름을 찾아서 Item 객체를 반환하고 싶다
+
+    // 상품을 상품명과 상세 설명을 or 조건을 이용해아 조회하는 쿼리 메서드
+    List<Item> findByItemNmOrItemDetail(String itemNm, String itemDetail);
+
+    List<Item> findByPriceLessThan(Integer price); //price 변수보다 값이 작은 상품을 조회
+
+    List<Item> findByPriceLessThanOrderByPriceDesc(Integer price); //price 변수보다 값이 작은 상품을 조회하고 가격순으로 내림차순
 
 
 }
