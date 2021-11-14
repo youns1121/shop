c550ebdc603d52a4e55ab7bab7fcbc4ba2b54e28
youns <minys94@daum.net>
2021-11-14 19:42:25 +0900

    기본 엔티티

---
+++ src/main/java/com/shop/entity/comm/BaseEntity.java
@@ -0,0 +1,32 @@
+package com.shop.entity.comm;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.Column;
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@MappedSuperclass
+@EntityListeners({AuditingEntityListener.class}) //해당 클래스에 Auditing 기능을 포함
+public class BaseEntity {
+
+    @CreatedDate // Entity가 생성되어 저장될 때 시간이 자동 저장
+    @Column(name="create_time", nullable = false)
+    private LocalDateTime createTime; //생성시간
+
+    @LastModifiedDate // 조회한 Entity의 값을 변경할 때 시간이 자동 저장
+    @Column(name="modifi_time", nullable = false)
+    private LocalDateTime modifiTime;  //수정시간
+
+    @Column(name="del_yn", nullable = false, columnDefinition = "VARCHAR(1) DEFAULT 'N'")
+    private String delYn; //삭제여부
+
+
+}
