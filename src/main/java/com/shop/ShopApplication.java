ddf58a42fb1e04f5fa7b37825ac2ccd63dc987bf
youns <minys94@daum.net>
2021-11-17 23:42:59 +0900

    Querydsl 초기설정

--- src/main/java/com/shop/ShopApplication.java
+++ src/main/java/com/shop/ShopApplication.java
@@ -1,8 +1,12 @@
 package com.shop;
 
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+import javax.persistence.EntityManager;
 
 @EnableJpaAuditing
 @SpringBootApplication // Spring Audit 기능을 활용하기 위해
@@ -10,6 +14,16 @@ public class ShopApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ShopApplication.class, args);
+    }
+
+    /**
+     *  JPAQueryFactory 스프링 빈으로 등록 하기
+     *  동시성 문제 없음
+     */
+
+    @Bean
+    JPAQueryFactory jpaQueryFactory(EntityManager em){
+        return new JPAQueryFactory(em);
     }
 
 }
