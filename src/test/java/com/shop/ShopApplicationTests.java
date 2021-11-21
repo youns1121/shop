4da22042a1a07261bf118e6610e8fedced4c5f84
youns <minys94@daum.net>
2021-11-21 14:25:12 +0900

    JPAQueryFactory를 이용한 상품 조회 예제

--- src/test/java/com/shop/ShopApplicationTests.java
+++ src/test/java/com/shop/ShopApplicationTests.java
@@ -1,6 +1,9 @@
 package com.shop;
 
+import com.querydsl.jpa.impl.JPAQuery;
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import com.shop.entity.Item;
+import com.shop.entity.QItem;
 import com.shop.enums.ItemSellStatus;
 import com.shop.repository.ItemRepository;
 import org.junit.jupiter.api.DisplayName;
@@ -9,6 +12,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import java.time.LocalDateTime;
 import java.util.List;
 
@@ -18,6 +23,9 @@ class ShopApplicationTests {
 
     @Autowired
     ItemRepository itemRepository;
+
+    @PersistenceContext
+    EntityManager em; // 영속성 컨텍스트를 사용하기 위해, EntityManager 빈을 주입
 
     @Test
     @DisplayName("상품저장테스트")
@@ -114,6 +122,24 @@ class ShopApplicationTests {
         this.createItemListTest();
         List<Item> itemList = itemRepository.findByItemDetailByNative("테스트 상품 상세 설명");
         for (Item item: itemList) {
+            System.out.println(item.toString());
+        }
+    }
+
+    @Test
+    @DisplayName("Querydsl 조회테스트1")
+    public void queryDslTest(){
+        this.createItemListTest();
+        JPAQueryFactory queryFactory = new JPAQueryFactory(em);
+        QItem qitem = QItem.item; // Querydsl을 통해 쿼리르 생성하기 위해 플러그인을 통해 자동으로 생성된 QItem 객체를 이용
+        JPAQuery<Item> query = queryFactory.selectFrom(qitem)
+                .where(qitem.itemSellStatus.eq(ItemSellStatus.SELL))
+                .where(qitem.itemDetail.like("%"+"테스트 상품 상세 설명"+"%"))
+                .orderBy(qitem.price.desc());
+
+        List<Item> itemList = query.fetch(); // 조회결과 리스트 반환, fetch() 메서드 실행 시점에서 쿼리문이 실행
+
+        for(Item item : itemList){
             System.out.println(item.toString());
         }
     }
