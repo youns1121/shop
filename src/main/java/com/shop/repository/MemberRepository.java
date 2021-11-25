7c3c4372c476049eab7c2b1ec4780c738cdf70aa
youns <minys94@daum.net>
2021-11-26 00:17:07 +0900

    Member Repository

---
+++ src/main/java/com/shop/repository/MemberRepository.java
@@ -0,0 +1,7 @@
+package com.shop.repository;
+
+import com.shop.entity.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MemberRepository extends JpaRepository<Member, Long> {
+}
