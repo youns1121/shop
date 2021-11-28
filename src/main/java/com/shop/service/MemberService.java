3e9a18f6fb4188021cf93d1613755a51d313d545
youns <minys94@daum.net>
2021-11-28 18:07:52 +0900

    Member Service

---
+++ src/main/java/com/shop/service/MemberService.java
@@ -0,0 +1,31 @@
+package com.shop.service;
+
+import com.shop.entity.Member;
+import com.shop.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+
+    public Member saveMember(Member member){ /*회원 생성*/
+        validateDuplicateMember(member);
+
+        return memberRepository.save(member);
+    }
+
+
+    @Transactional(readOnly = true)
+     public void validateDuplicateMember(Member member){ /*중복 회원 검사*/
+
+        Member findMember = memberRepository.findByEmail(member.getEmail());
+        if(findMember != null){
+            throw new IllegalStateException("이미 가입된 회원입니다.");
+        }
+    }
+}
