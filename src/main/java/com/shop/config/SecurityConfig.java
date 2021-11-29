8bbf033bc089c2a23ec545081da7138bfe96213f
youns <minys94@daum.net>
2021-11-29 23:30:28 +0900

    로그인/로그아웃 기능 구현

--- src/main/java/com/shop/config/SecurityConfig.java
+++ src/main/java/com/shop/config/SecurityConfig.java
@@ -1,25 +1,54 @@
 package com.shop.config;
 
+import com.shop.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 
+
+    private final MemberService memberService;
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-
+        http.formLogin()
+                .loginPage("/members/login") //로그인 페이지 URL 설정
+                .defaultSuccessUrl("/") //로그인 성공시 이동할 URL 설정
+                .usernameParameter("email") // 로그인 시 사용할 파라미터 이름으로 email을 지정
+                .failureUrl("/members/login/error") //로그인 실패 시 이동할 URL 설정
+                .and()
+                .logout()
+                .logoutRequestMatcher(new AntPathRequestMatcher("/members/logout")) //로그아웃 URL을 설정
+                .logoutSuccessUrl("/"); // 로그아웃 성공 시 이동할 URL을 설정
     }
 
     @Bean
     public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
+    }
+
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception{
+        /**
+         * 인증은 AuthenticationManagerBuilder가 AuthenticationManager를 생성
+         * userDetailService를 구현하고 있는 객체로 memberService를 지정, 비밀번호를 암호화를 위해 passwordEncoder를 지정
+         */
+
+        auth.userDetailsService(memberService)
+                .passwordEncoder(passwordEncoder());
     }
 }
--- src/main/java/com/shop/service/MemberService.java
+++ src/main/java/com/shop/service/MemberService.java
@@ -3,12 +3,17 @@ package com.shop.service;
 import com.shop.entity.Member;
 import com.shop.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
-public class MemberService {
+public class MemberService implements UserDetailsService {
+    /*UserDetailsService : DB에서 회원 정보를 가져오는 역할을 담당*/
 
     private final MemberRepository memberRepository;
 
@@ -27,5 +32,21 @@ public class MemberService {
         if(findMember != null){
             throw new IllegalStateException("이미 가입된 회원입니다.");
         }
+    }
+
+    @Override
+    /*회원정보를 조회하여 사용자의 정보와 권한을 갖는 UserDetails 인터페이스를 반환*/
+    /*메소드를 오버라이딩, 로그인할 유저의 email을 파라미터로 전달받음*/
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException { /*메소드를 오버라이딩, */
+        Member member = memberRepository.findByEmail(email);
+
+        if(member == null){
+            throw new UsernameNotFoundException(email);
+        }
+        return User.builder() /*UserDetail을 구현하고 있는 User 객체를 반환*/
+                .username(member.getEmail())
+                .password(member.getPassword())
+                .roles(member.getMemberRole().toString())
+                .build();
     }
 }
