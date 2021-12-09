cdcf39083fff8896487d18d9734d14d70a51305f
youns <minys94@daum.net>
2021-12-10 01:50:32 +0900

    상품 등록 Controller

--- src/main/java/com/shop/controller/ItemController.java
+++ src/main/java/com/shop/controller/ItemController.java
@@ -1,12 +1,24 @@
 package com.shop.controller;
 
 import com.shop.dto.form.ItemFormDto;
+import com.shop.service.ItemService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
+import javax.validation.Valid;
+import java.util.List;
+
+@RequiredArgsConstructor
 @Controller
 public class ItemController {
+
+    private final ItemService itemService;
 
     @GetMapping(value = "/admin/item/new")
     public String itemForm(Model model){
@@ -14,6 +26,32 @@ public class ItemController {
         model.addAttribute("itemFormDto", new ItemFormDto());
 
         return "/item/itemForm";
+    }
+
+    @PostMapping(value = "/admin/item/new")
+    public String itemNew(@Valid ItemFormDto itemFormDto, BindingResult bindingResult, Model model,
+                          @RequestParam("itemImgFile") List<MultipartFile> itemImgFileList){
+
+        if(bindingResult.hasErrors()){// 상품 등록 시 필수 값이 없다면 다시 상품 등록 페이지로 전환합니다.
+            return "item/itemForm";
+        }
+
+        if(itemImgFileList.get(0).isEmpty() && itemFormDto.getId() == null){ // 상품 등록 시 첫 번째 이미지가 없다면 에러 메시지와 함께 상품 등록 페이지로 전환
+            model.addAttribute("errorMessage", "첫번째 상품 이미지는 필수 입력 값 입니다.");
+            return "item/itemForm";
+        }
+        try{
+            itemService.saveItem(itemFormDto, itemImgFileList); //상품 저장 로직을 호출합니다
+
+        }catch (Exception e){
+            model.addAttribute("errorMessage", "상품 등록 중 에러가 발생하였습니다.");
+            return "item/itemForm";
+        }
+
+
+        return "redirect:/"; // 상품이 정상적으로 등록되었다면 메인페이지로 이동;
+
+
     }
 
 }
