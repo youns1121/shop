c68d7bece22b8732c762411eca4e348641400d26
youns <minys94@daum.net>
2021-12-12 23:12:56 +0900

    상품 데이터 정보 전달 Dto 수정

--- src/main/java/com/shop/dto/form/ItemFormDto.java
+++ src/main/java/com/shop/dto/form/ItemFormDto.java
@@ -7,6 +7,8 @@ import lombok.Getter;
 import lombok.Setter;
 import org.modelmapper.ModelMapper;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -16,17 +18,23 @@ public class ItemFormDto {
 
     private Long id;
 
-    private String itemNm; //아이템 이름름
+    @NotBlank(message = "상품명은 필수 입력 값입니다.")
+    private String itemNm; //아이템 이름
 
+    @NotNull(message = "가격은 필수 입력 값입니다.")
     private Integer price;
 
+    @NotBlank(message = "이름은 필수 입력 값입니다.")
     private String itemDetail;
 
+    @NotNull(message = "재고는 필수 입력 값입니다.")
     private Integer stockNumber;
 
     private ItemSellStatus itemSellStatus;
 
-    private List<ItemImgDto> itemImgDtoList = new ArrayList<>();
+    private List<ItemImgDto> itemImgDtoList = new ArrayList<>(); // 상품 저장 후 수정할 때 상품 이미지 정보를 저장하는 리스트
+
+    private List<ItemFormDto> itemImgIds = new ArrayList<>(); // 상품 이미지 아이디를 저장하는 리스트, 수정시에 이미지 아이디를 담아둘 용도
 
     private static ModelMapper modelMapper = new ModelMapper();
 
@@ -36,6 +44,6 @@ public class ItemFormDto {
     }
 
     public static ItemFormDto of(Item item){
-        return modelMapper.map(item, ItemFormDto.class);
+        return modelMapper.map(item, ItemFormDto.class); // modelMapper를 이용하여 엔티티 객체와 Dto 객체 간의 데이터를 복사하여 복사한 객체를 반횐해주는 메서드
     }
 }
