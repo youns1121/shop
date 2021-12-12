f70d960aa0675ebb0cb1a41e89f13ba3f4e3d832
youns <minys94@daum.net>
2021-12-12 23:47:08 +0900

    상품 업데이트 추가

--- src/main/java/com/shop/controller/ItemController.java
+++ src/main/java/com/shop/controller/ItemController.java
@@ -69,4 +69,28 @@ public class ItemController {
         return "item/itemForm";
     }
 
+    @PostMapping(value = "/admin/item/{itemId}")
+    public String itemUpdate(@Valid ItemFormDto itemFormDto, BindingResult bindingResult, @RequestParam("itemImgFile")
+                             List<MultipartFile> itemImgFileList, Model model){
+
+        if(bindingResult.hasErrors()){
+            return "item/itemForm";
+        }
+
+        if(itemImgFileList.get(0).isEmpty() && itemFormDto.getId() == null){
+            model.addAttribute("errorMessage", "첫번째 상품 이미지는 필수 입력 값 입니다.");
+            return "item/itemForm";
+        }
+
+        try{
+            itemService.updateItem(itemFormDto, itemImgFileList); // 상품 수정 로직을 호출
+        } catch (Exception e) {
+            model.addAttribute("errorMessage", "상품 수정 에러가 발생하였습니다.");
+            return "item/itemForm";
+        }
+
+        return "redirect:/";
+    }
+
+
 }
