99692e8e9eb3b755090a7a1dee40344ef18cb8bf
youns <minys94@daum.net>
2021-12-12 23:25:10 +0900

    상품 업데이트 추가

--- src/main/java/com/shop/service/ItemService.java
+++ src/main/java/com/shop/service/ItemService.java
@@ -62,9 +62,23 @@ public class ItemService {
         itemFormDto.setItemImgDtoList(itemImgDtoList);
 
         return itemFormDto;
+    }
 
+    public Long updateItem(ItemFormDto itemFormDto, List<MultipartFile> itemImgFileList) throws Exception{
 
+        //상품 수정
+        Item item = itemRepository.findById(itemFormDto.getId())// 상품 등록 화면으로부터 전달 받은 상품 아이디를 이용하여 상품 엔티티를 조회
+                .orElseThrow(EntityNotFoundException::new);
+        item.updateItem(itemFormDto); // 상품 등록 화면으로 부터 전달받은 itemFormDto를 통해 상품 엔티티를 업데이트함
 
+        List<Long> itemImgIds = itemFormDto.getItemImgIds(); // 상품 이미지 아이디 리스트를 조회합니다.
 
+        //이미지 등록
+        for(int i = 0; i < itemImgFileList.size(); i++){
+            itemImgService.updateItemImg(itemImgIds.get(i), itemImgFileList.get(i)); // 상품 이미지를 업데이트하기 위해서 updateItemImg()
+            //메서드에 상품 이미지 아이디와, 상품 이미지 파일 정보를 파라미터로 전달
+        }
+
+        return item.getId();
     }
 }
