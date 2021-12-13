b5a82739519cc53d2630fadfb693fa5d19f7f094
youns <minys94@daum.net>
2021-12-13 22:52:55 +0900

    상품 조회 조건 DTO

---
+++ src/main/java/com/shop/dto/ItemSearchDto.java
@@ -0,0 +1,21 @@
+package com.shop.dto;
+
+import com.shop.enums.ItemSellStatus;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+
+public class ItemSearchDto {
+
+    private String searchDateType; // 현재 시간과 상품 등록일을 비교해서 상품 데이터를 조회
+
+    private ItemSellStatus itemSellStatus; // 상품의 판매상태를 기준으로 상품 데이터를 조회
+
+    private String searchBy; // 상품을 조회할 때 어떤 유형으로 조회할지 선택, itemNm: 상품명/ createdBy : 상품 등록자 아이디
+
+    private String searchQuery = ""; // 조회할 검색어 저장할 변수,
+
+
+}
