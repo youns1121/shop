32ad7f96569075efef10918c6051bd6bce3e3ef2
youns <minys94@daum.net>
2021-12-19 01:04:30 +0900

    메인페이지 상품 조회

--- src/main/java/com/shop/service/ItemService.java
+++ src/main/java/com/shop/service/ItemService.java
@@ -2,6 +2,7 @@ package com.shop.service;
 
 import com.shop.dto.ItemImgDto;
 import com.shop.dto.ItemSearchDto;
+import com.shop.dto.MainItemDto;
 import com.shop.dto.form.ItemFormDto;
 import com.shop.entity.Item;
 import com.shop.entity.ItemImg;
@@ -89,5 +90,11 @@ public class ItemService {
     public Page<Item> getAdminItemPage(ItemSearchDto itemSearchDto, Pageable pageable){
 
         return itemRepository.getAdminItemPage(itemSearchDto, pageable);
+    }
+
+    @Transactional(readOnly = true)
+    public Page<MainItemDto> getMainItemPage(ItemSearchDto itemSearchDto, Pageable pageable){
+
+        return itemRepository.getMainItemPage(itemSearchDto, pageable);
     }
 }
