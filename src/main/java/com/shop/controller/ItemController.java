81b8992238d46f224a677fa5330dd10507cf9e5f
youns <minys94@daum.net>
2021-12-19 19:03:26 +0900

    상품 상세 페이지 추가

--- src/main/java/com/shop/controller/ItemController.java
+++ src/main/java/com/shop/controller/ItemController.java
@@ -1,8 +1,16 @@
 package com.shop.controller;
 
+
+
+import com.shop.dto.ItemSearchDto;
 import com.shop.dto.form.ItemFormDto;
+import com.shop.entity.Item;
 import com.shop.service.ItemService;
 import lombok.RequiredArgsConstructor;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -15,6 +23,7 @@ import org.springframework.web.multipart.MultipartFile;
 import javax.persistence.EntityNotFoundException;
 import javax.validation.Valid;
 import java.util.List;
+import java.util.Optional;
 
 @RequiredArgsConstructor
 @Controller
@@ -58,7 +67,7 @@ public class ItemController {
     public String itemDtl(@PathVariable("itemId") Long itemId, Model model){
 
         try{
-            ItemFormDto itemFormDto = itemService.getItemdtl(itemId); // 조회한 상품 데이터를 모델에 담아서 뷰로 전달
+            ItemFormDto itemFormDto = itemService.getItemDtl(itemId); // 조회한 상품 데이터를 모델에 담아서 뷰로 전달
             model.addAttribute("itemFormDto", itemFormDto);
         } catch (EntityNotFoundException e){
             model.addAttribute("errorMessage", "존재하지 않는 상품 입니다.");
@@ -91,6 +100,34 @@ public class ItemController {
 
         return "redirect:/";
     }
+
+
+    @GetMapping(value = {"/admin/items", "/admin/items/{page}"}) // value에 상품 관리 화면 진입 시 url에 페이지 번호가 없는 경우와 페이지 번호가 있는 경우 2가지를 매핑
+    public String itemManage(ItemSearchDto itemSearchDto, @PathVariable("page") Optional<Integer> page, Model model){
+        Pageable pageable= PageRequest.of(page.isPresent() ? page.get(): 0, 3); // 첫 번째 파라미터로는 조회할 페이지 번호, 두 번째 파라미터로는 한 번에 가ㅣ지고 올 데이터 수, 페이지 번호가 없으면 0페이지를 조회
+        Page<Item> items = itemService.getAdminItemPage(itemSearchDto, pageable); // 조회 조건과 페이징 정보를 파라미터로 넘겨서 Page<Item> 객체를 반환 받음
+        model.addAttribute("items", items); // 조회한 상품 데이터 및 페이징 정보를 뷰에 전달
+        model.addAttribute("itemSearchDto", itemSearchDto); // 페이지 전환 시 기존 검색 조건을 유지한 채 이동할 수 있도록 뷰에 다시 전달
+        model.addAttribute("maxPage", 5); // 페이지 번호 최대 개수
+
+        return "item/itemMng";
+
+   }
+
+   @GetMapping(value = "/item/{itemId}")
+    public String itemDtl(Model model, @PathVariable("itemId") Long itemId){
+        ItemFormDto itemFormDto = itemService.getItemDtl(itemId);
+        model.addAttribute("itemFormDto", itemFormDto);
+
+        return "item/itemDtl";
+
+
+   }
+
+
+
+
+
 
 
 }
