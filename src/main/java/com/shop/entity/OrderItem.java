be1a96e6175efeac5e9829ccca861ea3dc00595a
youns <minys94@daum.net>
2021-12-19 22:58:52 +0900

    주문상품 생성 메서드 추가

--- src/main/java/com/shop/entity/OrderItem.java
+++ src/main/java/com/shop/entity/OrderItem.java
@@ -32,6 +32,22 @@ public class OrderItem extends BaseEntity {
     @Column(name="order_count", nullable = false)
     private int count; //수량
 
+    public static OrderItem createOrderItem(Item item, int count){
+
+        OrderItem orderItem = new OrderItem();
+        orderItem.setItem(item); // 주문할 상품
+        orderItem.setCount(count); // 주문 수량
+        orderItem.setOrderPrice(item.getPrice()); // 현재 시간 기준 상품 가격을 주문 가격으로 세팅, 상품 가격은 상황에 따라 달라짐 ex)쿠폰, 할인
+
+        item.removeStock(count); // 주문 수량만큼 상품의 재고 수량을 감소
+
+        return orderItem;
+    }
+
+    public int getTotalPrice(){ // 주문 가격과 주문 수량을 곱해서 해당 상품을 주문한 총 가격을 계산하는 메서드
+
+        return orderPrice * count;
+    }
 
 
 }
