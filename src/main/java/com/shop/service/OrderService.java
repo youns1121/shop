64ae56278c0edcd00e621da79809724f7e63ec13
youns <minys94@daum.net>
2021-12-20 23:02:52 +0900

    Order Service

---
+++ src/main/java/com/shop/service/OrderService.java
@@ -0,0 +1,47 @@
+package com.shop.service;
+
+import com.shop.dto.OrderDto;
+import com.shop.entity.Item;
+import com.shop.entity.Member;
+import com.shop.entity.Order;
+import com.shop.entity.OrderItem;
+import com.shop.repository.ItemRepository;
+import com.shop.repository.MemberRepository;
+import com.shop.repository.OrderRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityExistsException;
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class OrderService {
+
+    private final ItemRepository itemRepository;
+    private final MemberRepository memberRepository;
+    private final OrderRepository orderRepository;
+
+    public Long order(OrderDto orderDto, String email){
+        Item item = itemRepository.findById(orderDto.getItemId()) //주문 할 상품을 조회
+                .orElseThrow(EntityExistsException::new);
+
+        Member member = memberRepository.findByEmail(email); // 현재 로그인한 회원의 이메일 정보를 이용해서 회원 정보를 조회
+
+        List<OrderItem> orderItemList = new ArrayList<>();
+        OrderItem orderItem =
+                OrderItem.createOrderItem(item, orderDto.getCount()); // 주문할 상품 엔티티와 주문 수량을 이용하여 주문 상품 엔티티를 생성
+
+        orderItemList.add(orderItem);
+
+        Order order = Order.createOrder(member, orderItemList);// 회원 정보와 주문할 상품 리스트 정보를 이용하여 주문 엔티티를 생성
+        orderRepository.save(order); // 생성한 주문 엔티티를 저장
+
+        return order.getId();
+
+
+    }
+}
