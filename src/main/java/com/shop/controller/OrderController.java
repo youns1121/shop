15d57f6686bc62892151e8f1f32b480a61a74117
youns <minys94@daum.net>
2021-12-24 23:55:31 +0900

    구매이력 조회 추가

--- src/main/java/com/shop/controller/OrderController.java
+++ src/main/java/com/shop/controller/OrderController.java
@@ -1,21 +1,25 @@
 package com.shop.controller;
 
 import com.shop.dto.OrderDto;
+import com.shop.dto.OrderHisDto;
 import com.shop.service.OrderService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 
 import javax.validation.Valid;
 import java.security.Principal;
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequiredArgsConstructor
@@ -49,6 +53,21 @@ public class OrderController {
         return new ResponseEntity<Long>(orderId, HttpStatus.OK); // 결과값으로 생성된 주문 번호와 요청이 성공했다는 HTTP응 답 상 태 코드를 반환
         }
 
+        //구매이력 조회
+    @GetMapping(value = {"/orders", "/orders/{page}"})
+    public String orderHist(@PathVariable("page") Optional<Integer> pagem, Principal principal, Model model){
+
+        Pageable pageable = PageRequest.of(pagem.isPresent() ? pagem.get() : 0, 4); //한 번에 가지고 올 주문의 개수는 4개로 설정
+
+        Page<OrderHisDto> orderHisDtoList = orderService.getOrderList(principal.getName(), pageable); // 현재 로그인한 회원은 이메일과 페이징 객체를 파라미터로 전달하여 화면에 전달한 주문 목록 데이터를 리턴 값으로 받음
+
+        model.addAttribute("orders", orderHisDtoList);
+        model.addAttribute("page", pageable.getPageNumber());
+        model.addAttribute("maxPAge", 5);
+
+        return "order/orderHist";
+
+    }
 
 
 }
