fdcd9e624cc154148a638880eb23c780a4c376ed
youns <minys94@daum.net>
2021-12-26 00:30:40 +0900

    주문 취소 테스트

--- src/test/java/com/shop/service/OrderServiceTest.java
+++ src/test/java/com/shop/service/OrderServiceTest.java
@@ -1,11 +1,12 @@
 package com.shop.service;
 
 import com.shop.dto.OrderDto;
-import com.shop.entity.Item;
-import com.shop.entity.Member;
-import com.shop.entity.Order;
-import com.shop.entity.OrderItem;
+import com.shop.domain.Item;
+import com.shop.domain.Member;
+import com.shop.domain.Order;
+import com.shop.domain.OrderItem;
 import com.shop.enums.ItemSellStatus;
+import com.shop.enums.OrderStatus;
 import com.shop.repository.ItemRepository;
 import com.shop.repository.MemberRepository;
 import com.shop.repository.OrderRepository;
@@ -13,7 +14,6 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.security.config.web.servlet.oauth2.client.OAuth2ClientSecurityMarker;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityExistsException;
@@ -67,7 +67,7 @@ class OrderServiceTest {
         Member member = saveMember();
 
         OrderDto orderDto = new OrderDto();
-        orderDto.setCount(10);
+        orderDto.setCount(1); // 한 개 주문
         orderDto.setItemId(item.getId());
 
         Long orderId = orderService.order(orderDto, member.getEmail()); // 주문 로직 호출 결과 생성된 주문 번호를 orderID 변수에 저장
@@ -80,6 +80,31 @@ class OrderServiceTest {
         int totalPrice = orderDto.getCount() * item.getPrice();// 주문상품의 총 가격
 
         assertEquals(totalPrice, order.getTotalPrice()); // 주문한 상품의 총 가격과 데이터베이스에 저장된 상품의 가격을 비교해서 같으면 테스트 성공
+
+
+    }
+
+    @Test
+    @DisplayName("주문 취소 테스트")
+    public void cancelOrder(){
+
+        Item item = saveItem(); // 상품 데이터 생성, 상품 재고는 100개
+        Member member = saveMember(); //회원 데이터 생성
+
+        OrderDto orderDto = new OrderDto();
+        orderDto.setCount(10);
+        orderDto.setItemId(item.getId());
+
+        Long orderId = orderService.order(orderDto, member.getEmail());// 주문데이터 생성, 주문 개수는 총 10개
+
+        Order order = orderRepository.findById(orderId)
+                .orElseThrow(EntityExistsException::new); // 생성한 주문 엔티티를 조회
+
+        orderService.cancelOrder(orderId); //해당 주문을 취소
+
+        assertEquals(OrderStatus.CANCEL, order.getOrderStatus()); // 주문의 상태가 취소 상태라면 테스트 통과
+        assertEquals(100, item.getStockNumber()); // 취소 후 상품의 재고가 처음 재고 개수인 100개와 동일 하면 테스트 통과
+
 
 
     }
