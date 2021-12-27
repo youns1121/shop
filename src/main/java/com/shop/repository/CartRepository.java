a039fc2b15bd47c8bd72822c471b2ef43141e7d3
youns <minys94@daum.net>
2021-12-27 23:16:30 +0900

    현재 로그인한 Cart 엔티티 찾기 메소드 추가

--- src/main/java/com/shop/repository/CartRepository.java
+++ src/main/java/com/shop/repository/CartRepository.java
@@ -1,7 +1,9 @@
 package com.shop.repository;
 
-import com.shop.entity.Cart;
+import com.shop.domain.Cart;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CartRepository extends JpaRepository<Cart, Long> {
+
+    Cart findByMemberId(Long memberId); // 현재 로그인한 Cart 엔티티 찾기
 }
