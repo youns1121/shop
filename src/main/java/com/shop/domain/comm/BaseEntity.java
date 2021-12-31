1263b4d4b3743d5ce961b4071dc614260429a815
youns <minys94@daum.net>
2021-12-31 23:46:47 +0900

    BaseEntity

--- src/main/java/com/shop/domain/comm/BaseEntity.java
+++ src/main/java/com/shop/domain/comm/BaseEntity.java
@@ -1,16 +1,13 @@
-package com.shop.entity.comm;
+package com.shop.domain.comm;
 
 import lombok.Getter;
-import lombok.Setter;
 import org.springframework.data.annotation.CreatedBy;
-import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import javax.persistence.Column;
 import javax.persistence.EntityListeners;
 import javax.persistence.MappedSuperclass;
-import java.time.LocalDateTime;
 
 @Getter
 @MappedSuperclass
@@ -18,14 +15,15 @@ import java.time.LocalDateTime;
 public abstract class BaseEntity extends BaseTimeEntity{
 
     @CreatedBy
-    @Column(updatable = false)
+    @Column(name = "created_by", updatable = false)
     String createdBy;
 
     @LastModifiedDate
+    @Column(name = "modified_by")
     private String modifiedBy;
 
-    @Column(name="del_yn"/*, nullable = false*/, columnDefinition = "VARCHAR(1) DEFAULT 'N'")
-    private String delYn; //삭제여부
+//    @Column(name="del_yn", columnDefinition = "VARCHAR(1) DEFAULT 'N'")
+//    private String delYn; //삭제여부
 
 
 }
