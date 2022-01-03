7a592aeb21bf962d032f36f7526dd2356d45359b
youns <minys94@daum.net>
2022-01-03 23:57:20 +0900

    장바구니 리스트 JPQL 쿼리 메서드 추가

--- src/main/java/com/shop/repository/CartItemRepository.java
+++ src/main/java/com/shop/repository/CartItemRepository.java
@@ -1,10 +1,24 @@
 package com.shop.repository;
 
 import com.shop.domain.CartItem;
+import com.shop.dto.CartDetailDto;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface CartItemRepository extends JpaRepository<CartItem, Long> {
 
     //카트 아이디와 상품 아이디를 이용해서 상품이 장바구니에 들어가있는지 조회
     CartItem findByCartIdAndItemId(Long cartId, Long itemId);
+
+    @Query("select new com.shop.dto.CartDetailDto(ci.id, i.itemNm, i.price, ci.count, im.imgUrl) " + // DTO 클래스 생성자에 명시한 순
+        "from CartItem ci, ItemImg im " +
+        "join ci.item i " +
+        "where ci.cart.id = ci.item.id " +
+        "and im.item.id = ci.item.id " + // 장바구니에 담겨있는 상품의 대표 이미지만 가지고 오도록 조건문 작성
+        "and im.repImgYn = 'Y' " + // 장바구니에 담겨있는 상품의 대표 이미지만 가지고 오도록 조건문 작성
+        "order by ci.createTime desc")
+    List<CartDetailDto> findCartDetailDtoList(Long cartId);
+
 }
