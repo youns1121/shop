e217beb7b1795b9c77b4a5c0507a776660c5578a
youns <minys94@daum.net>
2022-01-04 23:45:31 +0900

    장바구니 페이지 이동 메서드 추가

--- src/main/java/com/shop/controller/CartController.java
+++ src/main/java/com/shop/controller/CartController.java
@@ -1,5 +1,6 @@
 package com.shop.controller;
 
+import com.shop.dto.CartDetailDto;
 import com.shop.dto.CartItemDto;
 import com.shop.service.CartService;
 import javafx.beans.binding.StringBinding;
@@ -7,8 +8,10 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -26,7 +29,6 @@ public class CartController {
     @PostMapping(value = "/cart")
     public @ResponseBody ResponseEntity order(@RequestBody @Valid CartItemDto cartItemDto,
                                               BindingResult bindingResult, Principal principal) {
-
         if (bindingResult.hasErrors()) { //장바구니에 담을 상품 정보를 받는 cartItemDto 객체에 데이터 바인딩 시 에러가 있는지 검사
 
             StringBuilder sb = new StringBuilder();
@@ -52,6 +54,19 @@ public class CartController {
         }
         // 결과값으로 생성된 장바구니 상품 아이디와 요청이 성공하였다는 Http 응답 상태 코드 반환
         return  new ResponseEntity<Long>(cartItemId, HttpStatus.OK);
+    }
+
+
+
+    //장바구니 페이지 이동
+    @GetMapping(value = "/cart")
+    public String orderHist(Principal principal, Model model){
+
+        List<CartDetailDto> cartDetailDtoList = cartService.getCartList(principal.getName()); // 현재 로그인한 사용자의 이메일 정보를 이용하여 장바구니 상품 정보를 조회
+        model.addAttribute("cartItems", cartDetailDtoList); // 조회한 장바구니 상품 정보를 뷰로 전달
+
+        return "cart/cartList";
+
     }
 
 
