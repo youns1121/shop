467f28fd08430613ef7f492a82e4ecfce3ffe794
youns <minys94@daum.net>
2022-01-12 22:36:59 +0900

    장바구니에서 주문 생성

--- src/main/java/com/shop/service/OrderService.java
+++ src/main/java/com/shop/service/OrderService.java
@@ -17,6 +17,7 @@ import org.springframework.transaction.annotation.Transactional;
 import org.thymeleaf.util.StringUtils;
 
 import javax.persistence.EntityExistsException;
+import javax.persistence.EntityNotFoundException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -89,11 +90,36 @@ public class OrderService {
         return true;
     }
 
+    /**
+     * 주문 취소
+     * @param orderId
+     */
     public void cancelOrder(Long orderId){
 
         Order order = orderRepository.findById(orderId)
                 .orElseThrow(EntityExistsException::new);
 
         order.cancelOrder(); // 주문 취소 상태로 변경하면 변경 감지 기능에 의해서 트랜잭션이 끝날 때 update 쿼리가 실행됨
+    }
+
+    // 장바구니에서 주문 생성
+    public Long orders(List<OrderDto> orderDtoList, String email){
+
+        Member member = memberRepository.findByEmail(email);
+        List<OrderItem> orderItemList = new ArrayList<>();
+
+        for(OrderDto orderDto : orderDtoList){ // 주문할 상품 리스트를 만들어줌
+            Item item = itemRepository.findById(orderDto.getItemId()).orElseThrow(EntityNotFoundException::new);
+
+            OrderItem orderItem = OrderItem.createOrderItem(item, orderDto.getCount());
+            orderItemList.add(orderItem);
+        }
+
+
+        Order order = Order.createOrder(member, orderItemList); // 현재 로그인한 회원과 주문 상품 목록을 이용하여 주문 엔티티를 만들어줌
+        orderRepository.save(order); // 주문 데이터 저장
+
+        return order.getId();
+
     }
 }
