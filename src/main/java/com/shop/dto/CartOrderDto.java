214dbf77e9cda7c2b15a7f11619084a081116551
youns <minys94@daum.net>
2022-01-14 23:37:53 +0900

    장바구니 상품 주문하기

--- src/main/java/com/shop/dto/CartOrderDto.java
+++ src/main/java/com/shop/dto/CartOrderDto.java
@@ -11,5 +11,5 @@ public class CartOrderDto {
 
     private Long cartItemId;
 
-    private List<CartDetailDto> cartOrderDtoList; // 장바구니에서 여러 개의 상품을 주문하므로 CardOrderDto 클래스가 자기 자신을 List로 가짐
+    private List<CartOrderDto> cartOrderDtoList; // 장바구니에서 여러 개의 상품을 주문하므로 CardOrderDto 클래스가 자기 자신을 List로 가짐
 }
