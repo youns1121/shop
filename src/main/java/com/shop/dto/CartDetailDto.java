8ac01cdc9dd15ee7b8280fb0a370a9c3c2ac2e16
youns <minys94@daum.net>
2022-01-14 23:44:18 +0900

    장바구니 상세 내역 Dto

--- src/main/java/com/shop/dto/CartDetailDto.java
+++ src/main/java/com/shop/dto/CartDetailDto.java
@@ -1,5 +1,14 @@
 package com.shop.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * 장바구니 상세 내역
+ */
+
+@Getter
+@Setter
 public class CartDetailDto {
 
     private Long cartItemId; //장바구니 상품 아이디
@@ -11,6 +20,7 @@ public class CartDetailDto {
     private int count; // 수량
 
     private String imgUrl; // 상품 이미지 경로
+
 
     public CartDetailDto(Long cartItemId, String itemNm, int price, int count, String imgUrl) { //장바구니 페이지에 전달할 데이터를 생성자의 파리미터로 만듬
         this.cartItemId = cartItemId;
